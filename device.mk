18e8720068d3e014a8ed6e6de5bbc02212e9d2b0
LuK1337 <priv.luk@gmail.com>
2025-01-19 22:37:29 +0100

    mido: Switch to common lights HAL

--- device.mk
+++ device.mk
@@ -255,7 +255,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi_mido
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
