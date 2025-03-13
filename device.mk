6a8ee9279cd4eb95e8e0c221c171c00c564d73d4
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-13 11:30:47 +0100

    mido: Drop old Android Go configurations
    * These break build on QPR2.
    
    Change-Id: Ib2a80892f18d117e27dd703fa74122b24d900dcf | AOSP

--- device.mk
+++ device.mk
@@ -219,10 +219,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
-# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
