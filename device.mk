4914a2b546af607ea868b3cea818813a7c161064
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-23 19:51:31 +0800

    mido: Migrate wcnss_service variables to soong
    Change-Id: I2191d331b6bf8897e32b4cb0031c2776333da8ad
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- device.mk
+++ device.mk
@@ -411,6 +411,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_wlan_dictionary.dat
 
+$(call soong_config_set_bool,wcnss_service,uses_qcom_wcnss_qmi,true)
+$(call soong_config_set,wcnss_service,wcnss_qmi_include_dir,$(LOCAL_PATH)/wifi/include)
+
 # Wifi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
