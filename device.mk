addf4320d39aa97ea09190bff163e8161c61cd6e
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-05-10 22:27:11 +0530

    Revert "mido: Bump Keymaster to V4.0"
    This reverts commit 9e6e88f133ff4c08c932565c2934f2017ff71201.

--- device.mk
+++ device.mk
@@ -261,11 +261,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor \
-    android.hardware.keymaster@4.0-service \
-    android.hardware.keymaster@4.0.vendor \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
